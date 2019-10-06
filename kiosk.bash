be2470b4be81bd1fd795c3a72d119e2f9566adfa
Sven Latham <36373036+svenlatham@users.noreply.github.com>
2019-10-06 14:36:31 +0100

    Update kiosk.bash

--- kiosk.bash
+++ kiosk.bash
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-sudo apt-get -y install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox
+sudo apt-get -y install --no-install-recommends xserver-xorg x11-xserver-utils xinit openbox fbi
 sudo apt-get -y install --no-install-recommends chromium-browser
+sudo cp /boot/openbox-autostart /etc/xdg/openbox/autostart
+sudo cp /boot/openbox-environment /etc/xdg/openbox/environment
+echo "[[ -z \$DISPLAY && \$XDG_VTNR -eq 1 ]] && startx -- -nocursor" >~/.bash_profile
+sudo cp /boot/splashscreen.png /etc/
+sudo cp /boot/asplashscreen /etc/init.d/asplashscreen
+sudo chmod a+x /etc/init.d/asplashscreen
+sudo systemctl enable asplashscreen
+sudo reboot
